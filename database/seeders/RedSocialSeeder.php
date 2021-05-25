63135be9d748591a520e1a2fc466dae5b72b9071
Adonay Barahona <36289584+Hardslay@users.noreply.github.com>
2021-05-24 18:26:32 -0600

    Creación de seeders
    Archivos sin contenido por el momento.
    
    Se utilizarán para el llenado de la base de datos de forma automática

---
+++ database/seeders/RedSocialSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class RedSocialSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}

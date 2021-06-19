9ce068bb29cf27438cfcb22cccdd9c5a1376e480
Adonay Barahona <36289584+Hardslay@users.noreply.github.com>
2021-06-19 02:34:45 -0600

    Estado ap default

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -37,7 +37,7 @@ class DatabaseSeeder extends Seeder
             SectorSeeder::class,
             UsuarioSeeder::class,
             EmpresaSeeder::class,
-            EstadoAplicacion::class,
+            EstadoAplicacionSeeder::class,
         ]);
     }
 }
